873f7020fb37a42dc705ea6bd818662086e1e197
Christian Rondeau <christian.rondeau@gmail.com>
2017-12-27 13:19:05 -0500

    Bump version number to 1.3

--- SikuliSharp/Properties/AssemblyInfo.cs
+++ SikuliSharp/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("1678bdd5-c04e-43d5-b16d-6902ff91090a")]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0.0")]
